f4fa1b71e27c40477c02c5c7c1a943c680e363bb
quazzer <natsamiss@gmail.com>
2024-04-01 20:09:50 +0700

    :sparkles: Finish get env from .env file

--- api/api/server/env.go
+++ api/api/server/env.go
@@ -1,8 +1,17 @@
 package server
 
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
 const EnvFileName = ".env"
 
+type IENV interface{}
+
 type ENVConfig struct {
+	LogLevel logrus.Level
+
 	Host    string `mapstructure:"host"`
 	ENV     string `mapstructure:"env"`
 	Service string `mapstructure:"service"`
@@ -13,4 +22,40 @@ type ENVConfig struct {
 	DBUser     string `mapstructure:"DB_USERNAME"`
 	DBPassword string `mapstructure:"DB_PASSWORD"`
 	DBPort     string `mapstructure:"DB_PORT"`
+}
+
+type ENVType struct {
+	config *ENVConfig
+}
+
+func NewEnv() IENV {
+	return NewENVPath(".")
+}
+
+func NewENVPath(path string) IENV {
+	viper.SetConfigName(EnvFileName)
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetEnvPrefix("APP")
+	viper.AutomaticEnv()
+	viper.ReadInConfig()
+	envKeys := []string{
+		"LOG_HOST",
+		"HOST", "ENV", "SERVICE",
+		"SENTRY_DSN", "DB_ENGINE", "DB_HOST",
+	}
+
+	for _, key := range envKeys {
+		viper.BindEnv(key)
+	}
+
+	env := &ENVConfig{}
+	err := viper.Unmarshal(env)
+	if err != nil {
+	}
+
+	env.LogLevel, _ = logrus.ParseLevel(viper.GetString("log_level"))
+	return &ENVType{
+		config: env,
+	}
 }
--- api/api/server/server.go
+++ api/api/server/server.go
@@ -1,13 +1,11 @@
 package server
 
 import (
-	"log"
-
 	"github.com/gofiber/fiber/v2"
 )
 
 func NewHTTPServer() {
 	app := fiber.New(fiber.Config{})
 	// TODO: need to update this to use env instead
-	log.Fatal(app.Listen("0.0.0.0:8000"))
+	env := NewEnv()
 }
