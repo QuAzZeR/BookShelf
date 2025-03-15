323a16774d058867b74c23e55cabdd859b1bde48
QuAzZeR <natsamiss@gmail.com>
2025-03-15 17:14:00 +0700

    :sparkles: Update database schema

--- api/migrations/20240309105303_createTable-book.ts
+++ api/migrations/20240309105303_createTable-book.ts
@@ -1,17 +1,20 @@
-import type { Knex } from "knex";
+import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  knex.schema.createTable("book", (table) => {
-    table.uuid("id").primary();
-    table.string("name").notNullable();
-    table.string("eng_name");
-    table.string("author").notNullable();
-    table.string("transalator").notNullable();
-    table.datetime("created_at");
-    table.datetime("updated_at");
+  knex.schema.createTable('book', (table) => {
+    table.uuid('id').primary();
+    table.string('name').notNullable();
+    table.string('eng_name');
+    table.string('author').notNullable();
+    table.string('transalator').notNullable();
+    table.string('cover_url').notNullable();
+    table.string('language');
+    table.string('genre').notNullable();
+    table.datetime('created_at');
+    table.datetime('updated_at');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  knex.schema.dropTableIfExists("book");
+  knex.schema.dropTableIfExists('book');
 }
--- api/schema.md
+++ api/schema.md
@@ -6,6 +6,9 @@
           string eng_name
           string author
           string translator
+          string cover_url
+          string language
+          string genre
           datetime created_at
           datetime updated_at
       }
